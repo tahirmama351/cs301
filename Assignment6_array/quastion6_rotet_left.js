1396bb4bfa6350bef187bb1095fd0c8d5015f2dc
tahirmama351 <tahirmama@gmail.com>
2020-08-29 07:21:25 -0500

    Assignmet6

--- Assignment6_array/quastion6_rotet_left.js
+++ Assignment6_array/quastion6_rotet_left.js
@@ -5,11 +5,11 @@ let orginal_arr = [17, 8, 9, 5, 20]
 
 function rotate_right(array) {
 
-     for (i = 0; i < array.length - 1; i++) {
-          let temp_arr = array[i]
-          array[i]=array[i+1]
-          array[i+1]= temp_arr          
-     }
-     console.log(array)
+      // split arginal_arr in to two
+     let new_orginal_arr1 = [8, 9, 5,20]
+     let new_orginal_arr2 = [17]
+     let rotated_arr= new_orginal_arr1.concat(new_orginal_arr2)
+     
+     console.log(rotated_arr)
 }
 rotate_right(orginal_arr);
